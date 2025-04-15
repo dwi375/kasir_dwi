06ccc761098fa622f457b9a9e6e52378f9d4ec92
dwi375 <dwikeren789@gmail.com>
2025-04-15 16:25:19 +0700

    Add files via upload

---
+++ kasir_dwi/config/login_proses.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+include 'koneksi.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    
+    $query = "SELECT * FROM user WHERE username = '$username'";
+    $result = mysqli_query($conn, $query);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        $user = mysqli_fetch_assoc($result);
+
+        if ($password == $user['password']) {
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['role'] = $user['role'];
+
+            header('Location: ../pages/dashboard.php');
+            exit();
+        } else {
+            echo "Username atau password salah.";
+        }
+    } else {
+        echo "Username atau password salah.";
+    }
+}
+?>
